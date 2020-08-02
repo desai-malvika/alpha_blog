e04de4039af5034953ed76b3493d11d38cd767c8
malvika <malvika@gmail.com>
2020-08-02 16:29:18 +0530

    add user restrictions at controller level for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
+  
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
+
 
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
@@ -14,11 +18,9 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
             if @user.update(user_params)
               flash[:notice] = "your account information was successfully updated"
               redirect_to @user
@@ -41,7 +43,18 @@ class UsersController < ApplicationController
 
     private
 
+    def set_user
+      @user = User.find(params[:id])
+    end 
+
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def require_same_user
+      if current_user != @user
+        flash[:alert] = "You can only edit your own account"
+        redirect_to @user
+      end
     end
 end
